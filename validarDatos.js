5ad47c382d8a693cc5fe79bd34be770e861431f0
Jose Manuel Postigo <jpossua176@g.educaand.es>
2025-12-10 00:53:14 +0100

    style: Adjust comment capitalization and block comment format. Ajuste de comentarios y el bloque del formato del comentario

--- validarDatos.js
+++ validarDatos.js
@@ -27,14 +27,13 @@ document.getElementById('form1').addEventListener("submit", function (event) {
 function validarDatos(idUser, password) {
     let valido = true;
 
-    // longitud entre 8 y 15 caracteres para el idUser
+    // Longitud entre 8 y 15 caracteres para el idUser
     if (idUser.length < 8 || idUser.length > 15) {
         valido = false;
         mostrarError('idUserHelp', 'El idUser debe tener entre 8 y 15 caracteres');
     }
 
-    /** 
-     * Longitud entre 6 y 15 caracteres para la contraseña y debe contener mayusculas, minusculas, números y caracteres especiales (menos pero no ‘ “ \ / < > = ( )
+    /* Longitud entre 6 y 15 caracteres para la contraseña y debe contener mayusculas, minusculas, números y caracteres especiales (menos pero no ‘ “ \ / < > = ( )
      * u otros caracteres que puedan ser parte de un script malicioso)
      */
     if (password.length < 6 || password.length > 15 || !/[A-Za-z0-9]/.test(password) || !/[!@#$%^&*_+=\-\[\]{};':",.?]/.test(password) || /[‘“\\/<>=()]/.test(password)) {
